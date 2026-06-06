5898911c5d38b67ec1d5239388b63a0ac5db895f
Veyronix Admin <admin@veyronix.com>
2026-06-06 00:09:00 -0700

    revert(docs): remove fumadocs entirely and restore external wiki link

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,7 @@
-import { createMDX } from 'fumadocs-mdx/next';
-
-const withMDX = createMDX();
-
 /** @type {import('next').NextConfig} */
-const config = {
+const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['discord.js'], // If there was any previously needed or default
 };
 
-export default withMDX(config);
+export default nextConfig;
