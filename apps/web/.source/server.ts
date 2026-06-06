384503bab99a3cc8351501bf2b1817bbf4e4cbbe
Veyronix Admin <admin@veyronix.com>
2026-06-06 00:04:50 -0700

    fix(docs): move mdx files to root for correct fumadocs i18n routing

--- apps/web/.source/server.ts
+++ apps/web/.source/server.ts
@@ -1,12 +1,12 @@
 // @ts-nocheck
-import * as __fd_glob_7 from "../src/content/docs/tr/parti-yonetimi.mdx?collection=docs"
-import * as __fd_glob_6 from "../src/content/docs/tr/kurulum.mdx?collection=docs"
-import * as __fd_glob_5 from "../src/content/docs/tr/kurulum-komutlari.mdx?collection=docs"
-import * as __fd_glob_4 from "../src/content/docs/tr/index.mdx?collection=docs"
-import * as __fd_glob_3 from "../src/content/docs/en/setup.mdx?collection=docs"
-import * as __fd_glob_2 from "../src/content/docs/en/setup-commands.mdx?collection=docs"
-import * as __fd_glob_1 from "../src/content/docs/en/party-management.mdx?collection=docs"
-import * as __fd_glob_0 from "../src/content/docs/en/index.mdx?collection=docs"
+import * as __fd_glob_7 from "../src/content/docs/parti-yonetimi.mdx?collection=docs"
+import * as __fd_glob_6 from "../src/content/docs/parti-yonetimi.en.mdx?collection=docs"
+import * as __fd_glob_5 from "../src/content/docs/kurulum.mdx?collection=docs"
+import * as __fd_glob_4 from "../src/content/docs/kurulum.en.mdx?collection=docs"
+import * as __fd_glob_3 from "../src/content/docs/kurulum-komutlari.mdx?collection=docs"
+import * as __fd_glob_2 from "../src/content/docs/kurulum-komutlari.en.mdx?collection=docs"
+import * as __fd_glob_1 from "../src/content/docs/index.mdx?collection=docs"
+import * as __fd_glob_0 from "../src/content/docs/index.en.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
 
@@ -15,6 +15,6 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.doc("docs", "src/content/docs", {"en/index.mdx": __fd_glob_0, "en/party-management.mdx": __fd_glob_1, "en/setup-commands.mdx": __fd_glob_2, "en/setup.mdx": __fd_glob_3, "tr/index.mdx": __fd_glob_4, "tr/kurulum-komutlari.mdx": __fd_glob_5, "tr/kurulum.mdx": __fd_glob_6, "tr/parti-yonetimi.mdx": __fd_glob_7, });
+export const docs = await create.doc("docs", "src/content/docs", {"index.en.mdx": __fd_glob_0, "index.mdx": __fd_glob_1, "kurulum-komutlari.en.mdx": __fd_glob_2, "kurulum-komutlari.mdx": __fd_glob_3, "kurulum.en.mdx": __fd_glob_4, "kurulum.mdx": __fd_glob_5, "parti-yonetimi.en.mdx": __fd_glob_6, "parti-yonetimi.mdx": __fd_glob_7, });
 
 export const meta = await create.meta("meta", "src/content/docs", {});
